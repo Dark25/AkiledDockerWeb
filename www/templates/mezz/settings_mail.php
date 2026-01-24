f279187cafa3c092def3de91abf1ba89ae9747b4
Dark25 <nadiecaca2000@gmial.com>
2026-01-24 09:07:20 +0100

    Implementar soporte para tema oscuro en la página de configuración y crear archivo CSS correspondiente

--- www/templates/mezz/settings_mail.php
+++ www/templates/mezz/settings_mail.php
@@ -8,8 +8,8 @@ $emailS_active = 'active';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="/assets/styles/app.css">
-    <link rel="stylesheet" type="text/css" href="/assets/styles/settings_redesign.css">
+    <link rel="stylesheet" type="text/css" href="/assets/styles/settings_redesign-dark.css" media="(prefers-color-scheme: dark)">
+    <link rel="stylesheet" type="text/css" href="/assets/styles/settings_redesign.css" media="(prefers-color-scheme: light)">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <title><?= $config['hotelName'] ?>: <?= $lang["TittleHader5"] ?></title>
 </head>
